723271224b201ac5c5602dbf6527bd7df135dbbd
devisions <marius.ileana@gmail.com>
2020-10-23 21:24:11 +0300

    testing on business logic

---
+++ internal/product/product_test.go
@@ -0,0 +1,52 @@
+package product_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/devisions/garagesale/internal/product"
+	"github.com/devisions/garagesale/internal/schema"
+	"github.com/devisions/garagesale/internal/tests"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestProducts(t *testing.T) {
+
+	db, teardown := tests.NewUnit(t)
+	defer teardown()
+
+	np := product.NewProduct{Name: "Comic Books", Cost: 10, Quantity: 20}
+	now := time.Now().UTC()
+
+	saved, err := product.Create(db, np, now)
+	if err != nil {
+		t.Fatalf("could not create product: %v", err)
+	}
+
+	fetched, err := product.Retrieve(db, saved.ID)
+	if err != nil {
+		t.Fatalf("could not retrieve product: %v", err)
+	}
+
+	if diff := cmp.Diff(saved, fetched); diff != "" {
+		t.Fatalf("fetched product did not match saved. diff: %v", diff)
+	}
+}
+
+func TestProductList(t *testing.T) {
+
+	db, teardown := tests.NewUnit(t)
+	defer teardown()
+
+	if err := schema.Seed(db); err != nil {
+		t.Fatal(err)
+	}
+
+	ps, err := product.List(db)
+	if err != nil {
+		t.Fatalf("failed on listing products: %s", err)
+	}
+	if exp, got := 2, len(ps); exp != got {
+		t.Fatalf("expected product list size %v, got %v", exp, got)
+	}
+}
